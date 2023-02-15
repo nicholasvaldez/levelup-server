b2c2725db8374345700d858adcb8f2017f42e8d1
Nick Valdez <nicholastvaldez@gmail.com>
2023-02-15 11:15:15 -0600

    add update class to game and event

--- levelupapi/views/game.py
+++ levelupapi/views/game.py
@@ -15,11 +15,13 @@ class GameView(ViewSet):
         Returns:
             Response -- JSON serialized games
         """
-        # ORM gets single game from database base on primary key in the url
+        # * ORM gets single game from database base on primary key in the url
         game = Game.objects.get(pk=pk)
-        # sets serialized (converts data into a JSON string) data to variable
+
+        # * sets serialized (converts data into a JSON string) data to variable
         serializer = GameSerializer(game)
-        # pass variable to the response as the body
+
+        # * pass variable to the response as the body
         return Response(serializer.data)
 
     def list(self, request):
@@ -28,11 +30,13 @@ class GameView(ViewSet):
         Returns:
             Response -- JSON serialized list of games
         """
-        # ORM method .all() gets the whole collection of objects from database
+        # * ORM method .all() gets the whole collection of objects from database
         game = Game.objects.all()
-        # sets serialized (converts data into a JSON string) data to variable, many=true  because the response is a tuple/list
+
+        # * sets serialized (converts data into a JSON string) data to variable, many=true  because the response is a tuple/list
         serializer = GameSerializer(game, many=True)
-        # pass variable to the response as the body
+
+        # * pass variable to the response as the body
         return Response(serializer.data)
 
     def create(self, request):
@@ -42,12 +46,14 @@ class GameView(ViewSet):
             Response -- JSON serialized game instance
         """
         gamer = Gamer.objects.get(
-            user=request.auth.user)  # we use the request.auth.user to get the Gamer object based on the user
-        # retrieve game_type object from db
-        game_type = GameType.objects.get(pk=request.data["game_type"])
-        # The data passed in from the client is held in the request.data dictionary. Whichever keys are used on the request.data must match what the client is passing to the server.
+            user=request.auth.user)  # * we use the request.auth.user to get the Gamer object based on the user
 
-        game = Game.objects.create(  # ORM .create method: pass the fields as parameters to the function
+        # * retrieve game_type object from db
+        game_type = GameType.objects.get(pk=request.data["game_type"])
+
+        # * The data passed in from the client is held in the request.data dictionary. Whichever keys are used on the request.data must match what the client is passing to the server.
+
+        game = Game.objects.create(  # * ORM .create method: pass the fields as parameters to the function
             name=request.data["name"],
             maker=request.data["maker"],
             number_of_players=request.data["number_of_players"],
@@ -58,13 +64,34 @@ class GameView(ViewSet):
         serializer = GameSerializer(game)
         return Response(serializer.data)
 
+    def update(self, request, pk):
+        """Handle PUT requests for a game
 
-# The serializer class determines HOW the Python data should be serialized (converted) to be sent back to the client.
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+
+        # * get the object we want from the database
+        game = Game.objects.get(pk=pk)
+        # *the next lines are setting the fields on obj to the values coming from the client
+        game.name = request.data["name"]
+        game.maker = request.data["maker"]
+        game.number_of_players = request.data["number_of_players"]
+        game.skill_level = request.data["skill_level"]
+
+        game_type = GameType.objects.get(pk=request.data["game_type"])
+        game.game_type = game_type
+        game.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+
+# * The serializer class determines HOW the Python data should be serialized (converted) to be sent back to the client.
 class GameSerializer(serializers.ModelSerializer):
     """JSON serializer for games
-    """
+"""
     class Meta:  # holds the configuration for the serializer
         model = Game  # tell the serializer to use the game model
         # and to include id and the label fields
         fields = ('id', 'name', 'game_type', 'gamer',
-                  'maker', 'number_of_players', 'skill_level')
+                  'maker', 'number_of_players', 'skill_level',)
