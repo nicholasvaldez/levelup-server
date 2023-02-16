3bf257aee1d63a98e6c536d8dae28034c8533d91
Nick Valdez <nicholastvaldez@gmail.com>
2023-02-16 11:42:07 -0600

    user can join and leave events via custom actions

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -15,3 +15,11 @@ class Event(models.Model):
     description = models.CharField(max_length=250)
     attendees = models.ManyToManyField(
         "Gamer", through="attendance", related_name="attendees")
+
+    @property
+    def joined(self):
+        return self.__joined
+
+    @joined.setter
+    def joined(self, value):
+        self.__joined = value
